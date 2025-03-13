a4ff8661a2e806309e6b5cecf96db2553df22c10
Isabel-GarciaJ <issogajo910@gmail.com>
2025-03-12 20:51:49 -0500

    Primer commit

---
+++ ProyectoClase.py
@@ -0,0 +1,7 @@
+#Construir los siguientes métodos para dicha lista:
+
+#Verificar si la lista está vacía
+#Contar la cantidad de elementos existentes en la lista
+#Imprimir en pantalla los elementos de la lista
+#Agregar un elemento (Solo se agregaran por el inicio de la lista)
+#Buscar un elemento en la lista usando un método de ordenamiento (a libre elección del grupo)
