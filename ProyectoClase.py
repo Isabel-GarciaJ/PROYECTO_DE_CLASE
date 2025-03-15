0097eb38e543ad0c88bf4714076a6b4e6aaecde4
Solano-Sara <sara2240058@correo.uis.edu.co>
2025-03-14 21:49:39 -0500

    update 2
    falta implementar un algoritmo de busqueda, recomiendo usar los numeros de pedidos, tambien creo que la profe no se refería a de ordenamiento sino de busqueda,

--- ProyectoClase.py
+++ ProyectoClase.py
@@ -1,15 +1,45 @@
+import math
+
 class Nodo:
-	def __init__(self, data):
-		self.data = data
+	def __init__(self, pedido):
 		self.siguiente = None
+		self.pedido = pedido
 		
 	def imprimir(self):
-		d = str (self.data)
-		return d
+		print(self.pedido.imprimir())
+	
+class Cliente:
+	def __init__(self, nombre):
+		self.nombre = nombre
+		self.direccion = None
+		self.distancia = None
+	
+	def setdireccion(self):
+		calle = int(input("Introduzca la calle"))
+		carrera = int(input("Introduzca la carrera"))
+		distancia = [calle, carrera]
+		self.direccion = str("Calle ", calle, " #", carrera)
+		self.distancia = distancia
+
+class Pedido:
+	def __init__ (self, cliente, listproductos, numorden):    # clase pedido, tiene un cliente, productos a comprar y el numero del pedido
+		self.cliente = cliente
+		self.listproductos = listproductos
+		self.numorden = numorden
+	
+	def imprimir(self):
+		string = (
+            f"--- Pedido {self.numorden} ---\n"
+            f"Cliente: {self.cliente.nombre}\n"
+            f"Compra: {self.listproductos}\n"
+            f"Dirección: {self.cliente.direccion}"
+        )
+		return string
 
 class Lista:
 	def __init__ (self):
 		self.cabeza = None
+		self.local = [0,0]		#tomamos el local como punto de referencia
 	
     #Verificar si la lista está vacía
 	def vacia(self):
@@ -37,13 +67,35 @@ class Lista:
 			print("La lista esta vacia")
 		else:
 			for i in range (1,self.length()+1):
-				string = str(i) + ". " + nodo.imprimir()
-				print(string)	
+				print(print(f"{i}. {nodo.imprimir()}"))	
+				nodo = nodo.siguiente
 	
 	#Agregar un elemento (Solo se agregaran por el inicio de la lista)
-	def addNodo(self,data):
-		newNodo = Nodo(data)
+	def addNodo(self,pedido):
+		newNodo = Nodo(pedido)
 		newNodo.siguiente = self.cabeza
 		self.cabeza = newNodo
-		
-#Buscar un elemento en la lista usando un método de ordenamiento (a libre elección del grupo)
+
+	#Buscar un elemento en la lista usando un método de ordenamiento (sort)
+
+
+	
+	#ordenar la lista de menor a mayor distancia
+	def insertion_sort(lista, distancia):
+		for i in range(1, len(lista)):
+			clave = lista[i]
+			j = i - 1
+			while j >= 0 and distancia(lista[j]) > distancia(clave):
+				lista[j + 1] = lista[j]
+				j -= 1
+			lista[j + 1] = clave
+		return lista
+
+	#calcular la distancia del cliente al local
+	def _calcular_distancia(self, nodo):
+		x, y = self.local 
+		a, b = nodo.pedido.cliente.distancia
+		dx = a - x
+		dy = b - y
+		return math.sqrt(dx**2 + dy**2)  # Raíz cuadrada para obtener la distancia
+ 
