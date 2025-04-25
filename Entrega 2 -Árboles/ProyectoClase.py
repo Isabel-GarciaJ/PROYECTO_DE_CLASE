67878578be52a9ebbec2b4ec29157e93ea5ad1de
Isabel-GarciaJ <issogajo910@gmail.com>
2025-04-25 15:07:42 -0500

    Commit uno

---
+++ Entrega 2 -Árboles/ProyectoClase.py
@@ -0,0 +1,22 @@
+#Big tree
+#find_attr y find_attrs: Busca uno o varios nodos por atributo.
+
+#find_name y find_names: Busca uno o varios nodos por nombre.  
+
+# Arbol no binario, empleados fijos (repartidor). Altura 3
+    #Metodo para peso de las rutas
+    #Metodo para decidir que repatidores pueden realizar y que se escoja el de una capacidad más cercana
+    #agregar nuevas rutas
+    #Eliminar pedido ya recorrido
+    #metodo para buscar nodo (numero pedido) con big tree find_name
+    #Impripmir
+
+#Clase Nodo, capacidad(peso), nodos hijos, pedido(numero de pedido) y metodo para cambiar el pedido
+
+#Pedido (Si se agregan productos usar lectura de archivos),(numero de pedido), peso (si se usan productos, metodo para calcular peso), (precio a cobrar no seeeeee)
+    #imprimir
+    #chequear pedido (Una vez pasa el repartidor se coloca peso en cero y pedido en none)
+
+#Repartidor: nombre, capacidad y vehiculo,(Lista con numero de pedidos)
+
+#Demo: Rutas ya establecidas, metodo para agregar rutas
