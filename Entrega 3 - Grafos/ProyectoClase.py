2b7c83bc6a37002740c9ffdb47e8ce4ef0f94960
Isabel-GarciaJ <issogajo910@gmail.com>
2025-05-23 20:13:47 -0500

    Update ProyectoClase.py

--- Entrega 3 - Grafos/ProyectoClase.py
+++ Entrega 3 - Grafos/ProyectoClase.py
@@ -1,5 +1,6 @@
 from collections import deque
 import math
+import heapq
 
 class Grafo(object):
     def __init__(self):
@@ -61,66 +62,43 @@ def eliminar_sucursal_arista(grafo, nombre):
 
 # Buscar elementos
 # metodo para encontrar el camino minimo para entregar pedidos
-def agregar(grafo, elemento):
-    grafo.relaciones.update({elemento:[]})
 
-def relacionar(grafo, elemento1, elemento2, peso = 1):
-    relacionarUnilateral(grafo, elemento1, elemento2, peso)
-    relacionarUnilateral(grafo, elemento2, elemento1, peso)
+def camino_minimo(grafo, origen, destino):
+    distancias = {nodo: float('inf') for nodo in grafo.sucursales.values()}
+    previos = {nodo: None for nodo in grafo.sucursales.values()}
+    distancias[grafo.sucursales[origen]] = 0
+    
+    cola = [(0, grafo.sucursales[origen])]  # (distancia, nodo)
 
-def relacionarUnilateral(grafo, origen, destino, peso):
-    grafo.relaciones[origen].append(Arista(destino, peso))
+    while cola:
+        distancia_actual, nodo_actual = heapq.heappop(cola)
 
-def caminoMinimo(grafo, origen, destino):
-    etiquetas = {origen:(0,None)}
-    dijkstra(grafo, destino, etiquetas, [])
-    return construirCamino(etiquetas, origen, destino)
+        if nodo_actual.nombre == destino:
+            break
+        
+        if distancia_actual > distancias[nodo_actual]:
+            continue
+        
+        for arista in grafo.relaciones.get(nodo_actual, []):
+            vecino = arista.elemento
+            peso = arista.peso
+            distancia_nueva = distancia_actual + peso
+            
+            if distancia_nueva < distancias[vecino]:
+                distancias[vecino] = distancia_nueva
+                previos[vecino] = nodo_actual
+                heapq.heappush(cola, (distancia_nueva, vecino))
+    
+    # Reconstruir camino desde destino hacia origen
+    camino = []
+    actual = grafo.sucursales[destino]
+    while actual:
+        camino.append(actual)
+        actual = previos[actual]
+    camino.reverse()
+    
+    return camino, distancias[grafo.sucursales[destino]]
 
-def construirCamino(etiquetas, origen, destino):
-    if(origen == destino):
-        return [origen]
-    return construirCamino(etiquetas, origen, anterior(etiquetas[destino])) + [destino]
-
-
-def dijkstra(grafo, destino, etiquetas, procesados):
-    nodoActual = menorValorNoProcesado(etiquetas, procesados)
-#    print "-----------------------------"
-#    print "Nodo Actual:",nodoActual
-#    print "Procesados",procesados
-#    print "Etiquetas",etiquetas
-    if(nodoActual == destino):
-        return
-    procesados.append(nodoActual)
-    for vecino in vecinoNoProcesado(grafo, nodoActual, procesados):
-        generarEtiqueta(grafo, vecino, nodoActual, etiquetas)
-    dijkstra(grafo, destino, etiquetas, procesados)
-
-def generarEtiqueta(grafo, nodo, anterior, etiquetas):
-    etiquetaNodoAnterior = etiquetas[anterior]
-    etiquetaPropuesta = peso(grafo, anterior, nodo) + acumulado(etiquetaNodoAnterior),anterior
-    if(not(etiquetas.has_key(nodo)) or  acumulado(etiquetaPropuesta) < acumulado(etiquetas[nodo]) ):
-        etiquetas.update({nodo:etiquetaPropuesta})
-
-def aristas(grafo, nodo):
-    return grafo.relaciones[nodo]
-
-def vecinoNoProcesado(grafo, nodo, procesados):
-    aristasDeVecinosNoProcesados = filter(lambda x: not x in procesados, aristas(grafo,nodo))
-    return [arista.elemento for arista in aristasDeVecinosNoProcesados]
-
-
-def peso (grafo, nodoOrigen, nodoDestino):
-    return reduce(lambda x,y: x if x.elemento == nodoDestino else y, grafo.relaciones[nodoOrigen]).peso
-
-def acumulado(etiqueta):
-    return etiqueta[0]
-
-def anterior(etiqueta):
-    return etiqueta[1]
-
-def menorValorNoProcesado(etiquetas, procesados):
-    etiquetadosSinProcesar = filter(lambda (nodo,_):not nodo in procesados, etiquetas.iteritems())
-    return min(etiquetadosSinProcesar, key=lambda (_, (acum, __)): acum)[0]
 
 # Crear un grafo no dirigido
 g = Grafo()
